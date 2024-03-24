c959fe1d6fe25e9389d55a4a47b3a86d67a51f40
s5r <sakher.x@gmail.com>
2024-03-24 00:07:36 -0400

    run script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # TRAIN_FLAG = $1
 # CHECKPOINT_PATH = $2
-# DATA_PATH = $3 # data/webnlg
+# DATA_PATH = $3 # data/FB15K
 
 if [ $# -eq 0 ]
 then
@@ -12,19 +12,17 @@ fi
 if [ $1 == "train" ]
 then
 nohup python -u train.py \
-  --data_path $3/train.json \
-  --validation_path $3/valid.json \
-  --relations_path $3/rel2id.json \
+  --data_path $3 \
+  --structural_path $4 \
+  --text_path $5 \
   --train_size 1 \
-  --learning_rate 0.0015 \
-  --padding_size 100 \
-  --glove_path glove.6B.300d.txt \
+  --learning_rate 0.001 \
+  --padding_size 40 \
   --embedding_dimensions 300 \
-  --lstm_units 500 \
-  --pool_size 80 \
-  --strides 2 \
-  --dropout 0.15 \
-  --learning_decay 3e-5 \
+  --lstm_hidden_size 400 \
+  --lstm_hidden_lstm_layerssize 2 \
+  --dropout 0.2 \
+  --learning_decay 0.35 \
   --batch_size 32 \
   --epochs 50 \
   --patience 5 \
